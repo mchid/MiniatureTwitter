90ce84d3147893d2937e66cd98937d77cc858c3c
Muthu Chidambaram <MChiD@Muthus-MacBook-Pro.local>
2014-03-07 21:23:14 -0700

    Initial Commit

---
+++ MiniatureTwitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MiniatureTwitter
+//
+//  Created by Muthu Chidambaram on 3/7/14.
+//  Copyright (c) 2014 Muthu Chidambaram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MiniatureTwitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MiniatureTwitter
+//
+//  Created by Muthu Chidambaram on 3/7/14.
+//  Copyright (c) 2014 Muthu Chidambaram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
